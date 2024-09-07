663dfd513eecc06cb942e37760c9bb0830e4d0ae
r10k5 <r10k5.job@gmail.com>
2024-09-07 14:20:59 +0300

    feat(AppTiptap): add bubble-menu(bold, italic and strike)

--- src/components/AppTiptap.vue
+++ src/components/AppTiptap.vue
@@ -1,14 +1,52 @@
-<template>
-    <editor-content :editor="editor" />
-</template>
-
 <script setup lang="ts">
-import { useEditor, EditorContent } from '@tiptap/vue-3'
+import { useEditor, EditorContent, BubbleMenu, Editor } from '@tiptap/vue-3'
 import StarterKit from '@tiptap/starter-kit'
+import TipTapColor from '@tiptap/extension-color'
+import { onBeforeUnmount } from 'vue';
 
 const editor = useEditor({
     content: '<p>I’m running Tiptap with Vue.js. 🎉</p>',
-    extensions: [StarterKit],
+    extensions: [StarterKit, TipTapColor],
 })
+
+onBeforeUnmount(() => {
+  if (editor.value) {
+    editor.value.destroy();
+  }
+});
 </script>
-  
+
+<template>
+    <bubble-menu
+      :editor="editor"
+      :tippy-options="{ duration: 100 }"
+      v-if="editor"
+    >
+      <div class="bubble-menu">
+        <button @click="editor.chain().focus().toggleBold().run()" :class="{ 'is-active': editor.isActive('bold') }">
+          Bold
+        </button>
+        <button @click="editor.chain().focus().toggleItalic().run()" :class="{ 'is-active': editor.isActive('italic') }">
+          Italic
+        </button>
+        <button @click="editor.chain().focus().toggleStrike().run()" :class="{ 'is-active': editor.isActive('strike') }">
+          Strike
+        </button>
+      </div>
+    </bubble-menu>
+    <editor-content v-if="editor" :editor="editor" />
+</template>
+
+<style lang="scss">
+.bubble-menu {
+    background-color: rgba(255, 255, 255, 0.829);
+    display: flex;
+    gap: 2px;
+    button {
+        background-color: transparent;
+        border: none;
+        
+    }
+}
+</style>
+
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,31 @@
 <script setup lang="ts">
+import AppTiptap from '@/components/AppTiptap.vue';
+
+
 </script>
 
 <template>
   <div class="main-container">
+    <div class="field-container">
+      <AppTiptap/>
+    </div>
   </div>
 </template>
 
 <style scoped>
+.field-container {
+  width: 60%;
+  background-color: rgba(0, 0, 0, 0.267);
+  border: 2px solid black;
+  margin-top: 20px;
+}
 .main-container {
   width: 100%;
   height: 100%;
   background-color: rgb(43, 43, 102);
   margin: 0px 0px;
+
+  display: flex;
+  justify-content: center;
 }
 </style>
